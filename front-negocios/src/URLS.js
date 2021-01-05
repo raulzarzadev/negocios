255c8368b6f884865600dae853592c751add22d9
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-05 15:23:11 -0600

    Enviroment dev works

--- front-negocios/src/URLS.js
+++ front-negocios/src/URLS.js
@@ -1,6 +1,6 @@
-const PROD = process.env.NODE_ENV ==="production"
+const PROD = process.env.NODE_ENV !=="production"
 console.log(PROD)
 
 export const BACKEND_URL = PROD ? "http://localhost:3001/api": "http://138.68.233.95:4001"
 export const SIGNUP_SERVICE = PROD ? "http://localhost:3015/user": "http://138.68.233.95:4002"
-export const UPLOAD_IMAGE_SERVICE = PROD? "http://localhost:4042": "http://138.68.233.95:4003"
+export const UPLOAD_IMAGE_SERVICE = PROD ? "http://localhost:4042": "http://138.68.233.95:4003"
