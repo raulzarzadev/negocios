a9d43c5742cdec149f53675adef07a20e37334ba
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-04 22:07:13 -0600

    hotfix

--- front-negocios/src/URLS.js
+++ front-negocios/src/URLS.js
@@ -6,8 +6,8 @@ export const UPLOAD_IMAGE_SERVICE =
 export const BACKEND_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3001/api"
-    //: "https://api.negociosdelbarrio.com/api";
-    :"http://localhost:4001/api"
+    : "https://api.negociosdelbarrio.com/api";
+    //:"http://localhost:4001/api"
 
 export const SIGNUP_SERVICE =
   process.env.NODE_ENV === "development"
