f0f625e5b1e05a0049a86674ef5ee66631085a8f
Raul Zarza <raulzarza.dev@gmail.com>
2021-01-05 18:09:04 -0600

    cambiando puertos

--- front-negocios/src/URLS.js
+++ front-negocios/src/URLS.js
@@ -1,6 +1,6 @@
 const PROD = process.env.NODE_ENV !=="production"
 console.log(PROD)
 
-export const BACKEND_URL = PROD ? "http://localhost:3001/api": "http://138.68.233.95:4001"
-export const SIGNUP_SERVICE = PROD ? "http://localhost:3015/user": "http://138.68.233.95:4002"
-export const UPLOAD_IMAGE_SERVICE = PROD ? "http://localhost:4042": "http://138.68.233.95:4003"
+export const BACKEND_URL = PROD ? "http://localhost:3001/api": "https://adverts.negociosdelbarrio.com"
+export const SIGNUP_SERVICE = PROD ? "http://localhost:3015/user": "https://sign.negociosdelbarrio.com"
+export const UPLOAD_IMAGE_SERVICE = PROD ? "http://localhost:4042": "https://images.negociosdelbarrio.com"
